41c53f0c0ad89495d057838bae3d9c7572eb77fa
MDaugaardDK <48121840+MDaugaardDK@users.noreply.github.com>
2020-10-29 08:48:03 +0100

    Add files via upload

---
+++ AddRightClickTools.ps1
@@ -0,0 +1,15 @@
+﻿# Run this script with admin rights
+$Add_Button_Key = "Write a name for the button"
+$command_Key = "command"
+$Shell_Registry_Key = "HKCR:\Directory\Background\shell"
+$AddAppli_Registry_Key = "HKCR:\Directory\Background\shell\$Add_Button_Key"
+$Cmd_Registry_Key = "HKCR:\Directory\Background\shell\$Add_Button_Key\command"
+$String_cmd = "C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -executionpolicy bypass -file C:\\ProgramData\\Enter_file_name.ps1 -NoExit -Command Set-Location -LiteralPath '%V'" 
+ 
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+New-Item -Path $Shell_Registry_Key -Name $Add_Button_Key
+New-Item -Path $AddAppli_Registry_Key -Name $command_Key
+Set-Item -Path $Cmd_Registry_Key -Value $String_cmd
+
+# Remember to copy your script to "C:\ProgramData\" ! And specify the filename in line 7.
+# Check this blog for more information http://www.systanddeploy.com/2018/08/unblock-your-downloaded-files-with.html
---
+++ Unblock_files.ps1
@@ -0,0 +1,3 @@
+﻿# Unblock_files.ps1# Copy this file in %programdata%
+$Current_Folder =(get-location).path 
+Get-ChildItem $Current_Folder -Recurse | Unblock-File
